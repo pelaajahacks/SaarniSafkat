f04ca5916c22099f9dd7de72ba82fcb57e67be36
pelaajahacks <ronttipontti.sundstrom2@gmail.com>
2022-10-27 01:03:17 +0300

    finally

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const Twitter = require('twitter');
 require('dotenv/config')
 const https = require("https");
 const CronJob=require('cron').CronJob;
-
+const stringify_safely = require("safe-json-stringify")
+const querystring = require("querystring")
+const detectCharacterEncoding = require('detect-character-encoding');
 
 const fiksu_ai = require('./rating');
-
 
 
 const apikey = process.env.apikey
@@ -22,26 +23,28 @@ var client = new Twitter({
 
 function postInfoDiscord(message) {
     var url = "https://discord.com/api/webhooks/1034590470287929444/ceWSrNB52x_v8vTH-YI9cSXhUfnvK6qDb7YrSEPxg0ZnjsnS7P7cBRoV-wF3gZRgDgzz"
-    var postData = encodeURIComponent(JSON.stringify({
+    var postData = querystring.stringify(JSON.parse(JSON.stringify({
         content: message,
         username: "Saarni Safkat",
         tts: false,
 
-    }));
+    })));
+    
 
     const options = {
         method: 'POST',
         headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
         'Content-Length': postData.length,
+
         },
-        timeout: 1000, // in ms
+        timeout: 69000, // in ms
     };
 
     return new Promise((resolve, reject) => {
         const req = https.request(url, options, (res) => {
         if (res.statusCode < 200 || res.statusCode > 299) {
-            console.log(res)
+
             return reject(new Error(`HTTP status code ${res.statusCode}`));
         };
 
@@ -49,6 +52,7 @@ function postInfoDiscord(message) {
         res.on('data', (chunk) => body.push(chunk));
         res.on('end', () => {
             const resString = Buffer.concat(body).toString();
+
             resolve(resString);
         });
         });
@@ -61,7 +65,7 @@ function postInfoDiscord(message) {
         req.destroy();
         reject(new Error('Request time out'));
         });
-
+        console.log(postData)
         req.write(postData);
         req.end();
   });
@@ -79,7 +83,14 @@ function postFoodList() {
             try {
                 let foodlist = JSON.parse(data).result.pageContext.menu.Days;
                 // do something with JSON
-                const dayOfWeekDigit = new Date().getDay();
+                
+                var d = new Date();
+                var utcDate = new Date(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate(), d.getUTCHours(), d.getUTCMinutes(), d.getUTCSeconds(), d.getUTCMilliseconds());
+                
+                console.dir(foodlist)
+                const dayOfWeekDigit = utcDate.getDay()
+                console.log(dayOfWeekDigit)
+                
                 
                 for(const food of foodlist[dayOfWeekDigit].Meals) {
                     
@@ -97,14 +108,7 @@ function postFoodList() {
 Super fiksu A.I sanoo ruokaa ${rating}
 
 Kasvis lounaaksi on tänään ${kasvis_lounas.split(",")[0]}. 
-Super fiksu A.I sanoo ruokaa ${rating}
-    `
-                let discord_msg = `Tanaan lounaaksi on ${syötävä_ruoka.split(",")[0]}.
-Super fiksu A.I sanoo ruokaa ${rating}
-
-Kasvis lounaaksi on tanaan ${kasvis_lounas.split(",")[0]}. 
-Super fiksu A.I sanoo ruokaa ${rating}
-                    `
+Super fiksu A.I sanoo ruokaa ${rating}`
                 var params = { status: foodtweet };
                 client.post('statuses/update', params, function(error, tweet, response) {
                     if (error) throw error;
@@ -120,7 +124,7 @@ Super fiksu A.I sanoo ruokaa ${rating}
                 }
                 );
                 console.log(foodtweet)
-                postInfoDiscord(discord_msg)
+                postInfoDiscord(foodtweet)
                 
             } catch (error) {
                 console.error(error.message);
@@ -137,11 +141,11 @@ Super fiksu A.I sanoo ruokaa ${rating}
 postInfoDiscord(`Bot has started again!`)
 const postFoodListEveryday = new CronJob({
 
-    cronTime: '00 00 00 * * * ',
+    cronTime: '01 00 * * *',
     onTick: function () {
         console.log("Posting foodlist!!!")
         postFoodList()
     },
     start: true,
-    runOnInit: true
+    runOnInit: false
 });
