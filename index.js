47606b3ad6d0d4fb20b6c157e53d564b02137d01
pelaajahacks <ronttipontti.sundstrom2@gmail.com>
2022-11-17 17:15:08 +0200

    ok

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ var client = new Twitter({
 });
 
 function postInfoDiscord(message) {
-    var url = "https://discord.com/api/webhooks/1034590470287929444/ceWSrNB52x_v8vTH-YI9cSXhUfnvK6qDb7YrSEPxg0ZnjsnS7P7cBRoV-wF3gZRgDgzz"
+    var url = process.env.webhookurl
     var postData = querystring.stringify(JSON.parse(JSON.stringify({
         content: message,
         username: "Saarni Safkat",
@@ -82,61 +82,59 @@ function postInfoDiscord(message) {
 
 function postFoodList() {
     https.get("https://kouluruoka.fi/page-data/menu/espoo_saarnilaaksonkoulu/page-data.json", (response) => {
-        let data = "";
-        response.on("data", (chunk) => {
-            data += chunk;
-        });
-        response.on("end", () => {
-            try {
-                let foodlist = JSON.parse(data).result.pageContext.menu.Days;
-                // do something with JSON
+            let data = "";
+            response.on("data", (chunk) => {
+                data += chunk;
+            });
+            response.on("end", () => {
+                try {
+                    let foodlist = JSON.parse(data).result.pageContext.menu.Days;
+                    // do something with JSON
 
-                var d = new Date();
-                var utcDate = new Date(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate(), d.getUTCHours(), d.getUTCMinutes(), d.getUTCSeconds(), d.getUTCMilliseconds());
+                    var d = new Date();
+                    var utcDate = new Date(d.getUTCFullYear(), d.getUTCMonth(), d.getUTCDate(), d.getUTCHours(), d.getUTCMinutes(), d.getUTCSeconds(), d.getUTCMilliseconds());
 
-                console.dir(foodlist)
-                const dayOfWeekDigit = utcDate.getDay()
-                console.log(dayOfWeekDigit)
-
-
-                for (const food of foodlist[dayOfWeekDigit].Meals) {
+                    console.dir(foodlist)
+                    const dayOfWeekDigit = utcDate.getDay()
+                    console.log(dayOfWeekDigit)
 
 
-                    if (JSON.stringify(food.MealType).includes("KASVIS")) {
-                        var kasvis_lounas = food.Name
-                        var rating = fiksu_ai.get_rating(food.Name)
+                    for (const food of foodlist[dayOfWeekDigit].Meals) {
+
+
+                        if (JSON.stringify(food.MealType).includes("KASVIS")) {
+                            var kasvis_lounas = food.Name
+                            var rating = fiksu_ai.get_rating(food.Name)
+                        } else {
+                            var syötävä_ruoka = food.Name
+                            var rating = fiksu_ai.get_rating(food.Name)
+                        }
                     }
-                    else {
-                        var syötävä_ruoka = food.Name
-                        var rating = fiksu_ai.get_rating(food.Name)
-                    }
-                }
-                let foodtweet = `Tänään lounaaksi on ${syötävä_ruoka.split(",")[0]}.
+                    let foodtweet = `Tänään lounaaksi on ${syötävä_ruoka.split(",")[0]}.
 Super fiksu A.I sanoo ruokaa ${rating}
 
 Kasvis lounaaksi on tänään ${kasvis_lounas.split(",")[0]}. 
 Super fiksu A.I sanoo ruokaa ${rating}`
-                var params = { status: foodtweet };
-                client.post('statuses/update', params, function(error, tweet, response) {
-                    console.log(tweet);
-                    console.log(response.body);
-                    var tweetid = tweet.id_str
-                    console.log(tweetid);
-                    var params2 = { id: tweetid };
-                    client.post(`account/pin_tweet`, params2, function(error, pinned, response) {
-                        console.log(pinned)
+                    var params = { status: foodtweet };
+                    client.post('statuses/update', params, function(error, tweet, response) {
+                        console.log(tweet);
+                        console.log(response.body);
+                        var tweetid = tweet.id_str
+                        console.log(tweetid);
+                        var params2 = { id: tweetid };
+                        client.post(`account/pin_tweet`, params2, function(error, pinned, response) {
+                            console.log(pinned)
+                        });
                     });
-                }
-                );
-                console.log(foodtweet)
-                postInfoDiscord(foodtweet)
+                    console.log(foodtweet)
+                    postInfoDiscord(foodtweet)
 
-            } catch (error) {
-                console.log(error.message);
-            };
-        });
+                } catch (error) {
+                    console.log(error.message);
+                };
+            });
 
-    })
+        })
         .on("error", (error) => {
             console.log(error);
         });
